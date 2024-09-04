f75dd3015919e7fffa8d1183a06133142f96843d
nordinzakaria <nordinzakaria@gmail.com>
2024-09-04 11:43:00 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            SensorData sd = new SensorData(1000);
+
+            sd.init();
+
+
+            var watch = new System.Diagnostics.Stopwatch();
+    
+            watch.Start();
+            sd.average();
+            watch.Stop();
+            Console.WriteLine($"Execution Time for Average: {watch.ElapsedMilliseconds} ms");
+    }
+    }
+
---
+++ SensorData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SensorData
+  {
+    // data
+    int[] temp;
+    int NumDrones;
+    Random rnd = new Random();
+
+    public SensorData(int NumDrones) {
+        temp = new int[NumDrones];
+        this.NumDrones = NumDrones;
+    }
+
+    public void init()
+    {
+        for (int j = 0; j < NumDrones; j++)
+        {
+            temp[j] = rnd.Next(40);//returns random integers < 10
+        }
+    }
+
+    public float average()
+    {
+        return 0;
+    }
+
+    public int max()
+    {
+        return 0; 
+    }
+
+    public int min()
+    {
+        return 0;
+    }
+}
